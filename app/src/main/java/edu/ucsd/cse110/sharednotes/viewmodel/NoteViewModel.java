6bc7a29c8beeab6fd7325b16ea4ed9bbdc353bbb
Dylan Lukes <lukes.dylan@gmail.com>
2023-02-27 14:18:46 -0800

    add one more hint

--- app/src/main/java/edu/ucsd/cse110/sharednotes/viewmodel/NoteViewModel.java
+++ app/src/main/java/edu/ucsd/cse110/sharednotes/viewmodel/NoteViewModel.java
@@ -24,6 +24,10 @@ public class NoteViewModel extends AndroidViewModel {
 
     public LiveData<Note> getNote(String title) {
         // TODO: check if a newer version of the note exists on the server.
+        // TODO: set up polling background thread (Mutable/MediatorLiveData?)
+        // The returned live data should update whenever there is a change in
+        // the database, or when the server returns a newer version of the note.
+        // Polling interval: 3s.
         if (note == null) {
             note = repo.getLocal(title);
         }
